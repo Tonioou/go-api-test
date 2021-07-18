9eb89953e55d5e9bd2f8af4942bf3e61bd672daf
Antonio Marcos F. Silva <antoniomarcos997@gmail.com>
2021-07-17 21:10:53 -0300

    chore(api): remove ping endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -7,12 +7,6 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
 	api.NewPersonApi().Register(r)
 	r.Run(":8080")
 }
