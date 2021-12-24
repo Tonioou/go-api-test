5d17f18f269e96658dd5dc2f41067f6264bb36d5
Antonio Fernandes <antoniomarcos997@gmail.com>
2021-12-23 23:42:25 -0300

    feat(config): override default values with env variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,12 +2,13 @@ package config
 
 import (
 	"fmt"
+	"strings"
 	"sync"
 
 	"github.com/spf13/viper"
 )
 
-type Config struct {
+type Configs struct {
 	Postgres struct {
 		Url string
 	}
@@ -15,30 +16,31 @@ type Config struct {
 }
 
 var configRunOnce sync.Once
-var config *Config
+var configs *Configs
+var config = viper.New()
 
-func GetConfig() *Config {
+func GetConfig() *Configs {
 	configRunOnce.Do(func() {
-		viper.SetConfigName("config")
-		viper.SetConfigType("yaml")
-		viper.AutomaticEnv()
+		config.SetDefault("log.level", "info")
+		config.SetDefault("postgres.host", "postgres")
+		config.SetDefault("postgres.port", 5432)
+		config.SetDefault("postgres.username", "teste")
+		config.SetDefault("postgres.password", "test")
+		config.SetDefault("postgres.database", "todo_list")
 
-		viper.SetDefault("log.level", "info")
-		viper.SetDefault("postgres.host", "postgres")
-		viper.SetDefault("postgres.port", 5432)
-		viper.SetDefault("postgres.username", "postgres")
-		viper.SetDefault("postgres.password", "postgres")
-		viper.SetDefault("postgres.database", "todo_list")
+		config.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+		config.AutomaticEnv()
 
 		databaseUrl := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
-			viper.GetString("postgres.username"),
-			viper.GetString("postgres.password"),
-			viper.GetString("postgres.host"),
-			viper.GetString("postgres.port"),
-			viper.GetString("postgres.database"),
+			config.GetString("postgres.username"),
+			config.GetString("postgres.password"),
+			config.GetString("postgres.host"),
+			config.GetString("postgres.port"),
+			config.GetString("postgres.database"),
 		)
 
-		config = &Config{
+		configs = &Configs{
 			Postgres: struct{ Url string }{
 				Url: databaseUrl,
 			},
@@ -46,5 +48,5 @@ func GetConfig() *Config {
 		}
 	})
 
-	return config
+	return configs
 }
