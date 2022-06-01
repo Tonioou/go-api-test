5fededb717c83885a91dc9489af56244b563d78c
Antonio Fernandes <antoniomarcos997@gmail.com>
2022-06-01 01:03:28 -0300

    chore(otel): add trace on other methods

--- internal/service/todo.go
+++ internal/service/todo.go
@@ -8,6 +8,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/joomcode/errorx"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
 )
 
 type Todo interface {
@@ -31,6 +32,10 @@ func (tr *TodoService) GetById(ctx context.Context, id uuid.UUID) (model.Todo, *
 	newCtx, span := otel.Tracer("service-todo").Start(ctx, "GetById")
 	defer span.End()
 	result, errx := tr.TodoRepository.GetById(newCtx, id)
+	if errx != nil {
+		span.RecordError(errx)
+		span.SetStatus(codes.Error, errx.Error())
+	}
 	return result, errx
 }
 
@@ -39,6 +44,10 @@ func (tr *TodoService) Save(ctx context.Context, addTodo *command.AddTodo) (mode
 	defer span.End()
 	todo := model.NewTodo(addTodo.Name)
 	result, errx := tr.TodoRepository.Save(newCtx, todo)
+	if errx != nil {
+		span.RecordError(errx)
+		span.SetStatus(codes.Error, errx.Error())
+	}
 	return result, errx
 }
 
@@ -47,9 +56,15 @@ func (tr *TodoService) Update(ctx context.Context, updateTodo *command.UpdateTod
 	defer span.End()
 	_, errx := tr.TodoRepository.GetById(newCtx, updateTodo.Id)
 	if errx != nil {
+		span.RecordError(errx)
+		span.SetStatus(codes.Error, errx.Error())
 		return model.Todo{}, errx
 	}
 	todo, errx := tr.TodoRepository.Update(newCtx, updateTodo)
+	if errx != nil {
+		span.RecordError(errx)
+		span.SetStatus(codes.Error, errx.Error())
+	}
 	return todo, errx
 }
 
@@ -58,6 +73,8 @@ func (tr *TodoService) Delete(ctx context.Context, id uuid.UUID) *errorx.Error {
 	defer span.End()
 	_, errx := tr.TodoRepository.GetById(newCtx, id)
 	if errx != nil {
+		span.RecordError(errx)
+		span.SetStatus(codes.Error, errx.Error())
 		return errx
 	}
 	return tr.TodoRepository.Delete(newCtx, id)
