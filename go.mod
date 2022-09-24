19e4435d9624f9a6da29794849d8139df285ff95
Antonio Fernandes <antoniomarcos997@gmail.com>
2022-09-24 15:49:10 -0300

    chore(go-version): update to 1.19

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/Tonioou/go-todo-list
 
-go 1.18
+go 1.19
 
 require (
 	github.com/google/uuid v1.3.0
--- internal/api/todo.go
+++ internal/api/todo.go
@@ -56,6 +56,7 @@ func (ta *TodoApi) GetById(c echo.Context) error {
 		span.SetStatus(codes.Error, errx.Error())
 		return c.JSON(errorResponse.StatusCode, errorResponse)
 	}
+	//otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(c.Response().Header()))
 	return c.JSON(200, result)
 }
 
